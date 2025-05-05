f12184e500503cbdd7b3cb5d56e5c8f33b43973b
efchbd1013 <efchbd1013@gmail.com>
2025-05-05 15:04:29 +0300

    Adding client port on cloud

--- Project/Program.cs
+++ Project/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://localhost:3000",
-                              "https://education-for-all-frontend.onrender.com").
+                              "https://education-for-all.onrender.com").
                           AllowAnyMethod().
                           AllowAnyHeader().
                           AllowCredentials().
