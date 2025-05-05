a0420266ae5f352b7d1b2e5472759c6d76cf7152
efchbd1013 <efchbd1013@gmail.com>
2025-05-05 16:38:30 +0300

    Try to recognize frontend

--- Project/Program.cs
+++ Project/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy.WithOrigins("http://localhost:3000",
-                              "https://education-for-all.onrender.com").
+                              "https://education-for-all.onrender.com/api").
                           AllowAnyMethod().
                           AllowAnyHeader().
                           AllowCredentials().
